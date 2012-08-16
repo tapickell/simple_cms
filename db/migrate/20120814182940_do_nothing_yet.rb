31ce935cdcd20563acf8a15b7fc8ad48cf57cf83
Todd Pickell <toddpickell@me.com>
2012-08-15 19:18:14 -0500

    starting chpt. 7 models, active record, active relation

---
+++ db/migrate/20120814182940_do_nothing_yet.rb
@@ -0,0 +1,9 @@
+class DoNothingYet < ActiveRecord::Migration
+  def up
+    # instructions for changing db to new state
+  end
+
+  def down
+    # instructions for changing db to previous state
+  end
+end
